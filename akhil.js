c4f522bce6fad88233d5c1b53985ee08e8397886
unknown <kodavathakhil.pc@gmail.com>
2024-10-02 13:06:17 +0530

    ep-01

---
+++ akhil.js
@@ -0,0 +1,3 @@
+const heading=React.createElement("h1",{class:"parent"},React.createElement("h1",{class:"child"},"hello am babu"));
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
